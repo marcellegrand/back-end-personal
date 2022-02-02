ec0beb516baaf571f064437d08d92cacb385a7a5
marcellegrand <marcel.lazodelavega@gmail.com>
2022-02-01 19:28:13 -0500

    Finishing: dia04, dia05

--- dia05/shop/api/views.py
+++ dia05/shop/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import *
+from tienda.models import *
 from .serializers import *
 
 class IndexView(APIView):
