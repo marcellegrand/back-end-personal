ba89ab225f6461030ac7c1f50f4a607a761e748a
marcellegrand <marcel.lazodelavega@gmail.com>
2022-03-08 22:39:22 -0500

    Adding factores.txt; finishing monedas.py

--- semana01/dia01/monedas.py
+++ semana01/dia01/monedas.py
@@ -1,12 +1,22 @@
+##############################################
+#CONVERSOR DE MONEDAS
+##############################################
+
 #ZONA DE IMPORTACIÓN
 import locale
+import os
+
+#CONFIGURACIONES BÁSICAS
 locale.setlocale(locale.LC_ALL,'es_PE.UTF-8')
 
-#CONVERSOR DE MONEDAS
-
 #DEFINICIÓN DE VARIABLES
-tipocambio_penusd = 4.067
-tipocambio_peneur = 4.570
+dicFactores = []
+tipocambio_penusd = 0
+tipocambio_peneur = 0
+indice = 0
+clave = ''
+valor = ''
+tupla = 1
 
 monedaorigen = 0
 monedadestino = 0
@@ -17,6 +27,43 @@ montofinal = 0
 reintentarmonedas = True
 reintentarmonto = True
 
+#APERTURA DEL ARCHIVO DE FACTORES DE CAMBIO
+if (os.path.isfile('factores.txt')):
+    filFactores = open('factores.txt','r')
+    strFactores = filFactores.read()
+    #penusd->3.780
+    #peneur->4.570
+    arrFactores = strFactores.splitlines()
+    #['penusd->3.780', 'peneur->4.570']
+    
+    for eleFactor in arrFactores:
+        comFactor = eleFactor.split('->')
+        #penusd
+        #3.780
+        #peneur
+        #4.570
+        dicFactor = {
+            'key':comFactor[0],
+            'value':comFactor[1]
+        }
+        dicFactores.append(dicFactor)
+    
+    filFactores.close()
+
+#SETEO DE FACTORES DE CAMBIO
+indice = 0
+while (indice < len(dicFactores)):
+    tupla = 1
+    for clave, valor in dicFactores[indice].items():
+        if (tupla == 2):
+            if (indice == 0): #PEN->USD
+                tipocambio_penusd = float(valor)
+            if (indice == 1): #PEN->EUR
+                tipocambio_peneur = float(valor)
+        tupla+=1 #tupla = tupla + 1
+    indice+=1 #indice = indice + 1
+
+#INICIO DEL PROGRAMA
 print("Monedas:")
 print("(1) PEN")
 print("(2) USD")
