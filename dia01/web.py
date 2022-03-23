ed966dc17a7e4be805adba801c39a75d7195d402
marcellegrand <marcel.lazodelavega@gmail.com>
2022-03-22 22:14:27 -0500

    Reviewing semana02/dia01 -> 2

--- dia01/web.py
+++ dia01/web.py
@@ -32,6 +32,9 @@ def substract(n1=0,n2=0):
 
 @app.route('/calculator',methods=['GET','POST'])
 def calculator():
+    '''
+    El action definido dentro de este form apunta a la ruta definida en este @app.route
+    '''
     form = "<form action = 'calculator' method = 'POST'>"
     form += "<input type = 'text' name = 'n1' size = '2'/> + <input type = 'text' name = 'n2' size = '2'/>"
     form += "<input type = 'submit' value='=' />"
@@ -47,41 +50,58 @@ def calculator():
  
 #Tarea: 20210104
 @app.route('/aritmetic',methods=['GET','POST'])
-def aritmetic():
-    op = 0
-    te = ""
-    
-    def operator(val):
-        op = val  
-    
+def aritmetic():   
+    operator = 0
+    number_1 = 0
+    number_2 = 0
+    default_sum = ""
+    default_sub = ""
+    default_mul = ""
+    default_div= ""
+
+    if request.method == 'POST':
+        number_1 = int(request.form['number_1'])
+        number_2 = int(request.form['number_2'])
+        operator = int(request.form['operator'])
+
+    if operator > 0:  
+        if operator == 1: 
+            result = number_1 + number_2
+            text = "suma"
+            default_sum = "checked"
+        if operator == 2: 
+            result = number_1 - number_2
+            text = "resta"
+            default_sub = "checked"
+        if operator == 3: 
+            result = number_1 * number_2
+            text = "multiplicación"
+            default_mul = "checked"
+        if operator == 4: 
+            if number_2 != 0:
+                result = number_1 / number_2
+                text = "división"
+            else:
+                result = 0
+                text = "#¡error!"
+            default_div = "checked"
+            
     form = "<form action = 'aritmetic' method = 'POST'>"
-    form += "<input type = 'text' name = 'n1' size = '2'/>"
-    form += "<button id = 'op_s' action = 'operator(1)'>+</button>"
-    form += "<button id = 'op_r' action = 'operator(2)'>-</button>"
-    form += "<button id = 'op_m' action = 'operator(3)'>*</button>"
-    form += "<button id = 'op_d' action = 'operator(4)'>/</button>"
-    form += "<input type = 'text' name = 'n2' size = '2'/>"
+    form += "<input type = 'text' name = 'number_1' size = '2' value = '" + str(number_1) + "'/>"
+    form += "<input type = 'radio' name = 'operator' value = '1' " + default_sum + "/> sumar "
+    form += "<input type = 'radio' name = 'operator' value = '2' " + default_sub + "/> restar "
+    form += "<input type = 'radio' name = 'operator' value = '3' " + default_mul + "/> multiplicar "
+    form += "<input type = 'radio' name = 'operator' value = '4' " + default_div + "/> dividir "
+    form += "<input type = 'text' name = 'number_2' size = '2' value = '" + str(number_2) + "'/>"
     form += "<input type = 'submit' value='=' />"
     form += "</form>"
     
-    if request.method == 'POST':
-        n1 = request.form['n1']
-        n2 = request.form['n2']
-        if op == 1: 
-            re = int(n1) + int(n2)
-            te = "suma"
-        if op == 2: 
-            re = int(n1) - int(n2)
-            te = "resta"
-        if op == 3: 
-            re = int(n1) * int(n2)
-            te = "multiplicación"
-        if op == 4: 
-            re = int(n1) / int(n2)
-            te = "división"
-            
-        form += '<h2>La ' + te + ' es {}</h2>'.format(re)
-    
+    if operator > 0:
+        if text == "#¡error!":
+            form += '<h2>' + text + '</h2>'
+        else:    
+            form += '<h2>La ' + text + ' es {}</h2>'.format(result)        
+        
     return form    
 
 app.run(debug = True)
